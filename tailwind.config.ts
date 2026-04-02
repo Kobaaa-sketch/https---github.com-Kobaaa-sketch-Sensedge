a644d13b6aea94e86147ec6ec8b799718d06be7b
Flori <sensedgeoff@gmail.com>
2026-04-02 18:43:20 +0200

    qsd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,3 +10,17 @@ export default {
   },
   plugins: [animate],
 };
+
+import animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate, typography],
+};
